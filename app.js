70832eda52220d393bc2c1b0bf69ccf189c3c7cf
Micheal Peter <79177019+myconpeter@users.noreply.github.com>
2024-05-30 14:13:46 +0100

    ommmo

--- app.js
+++ app.js
@@ -1,51 +1,43 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const mongoose= require('mongoose');
-const passport = require ('passport');
+const mongoose = require('mongoose');
+const passport = require('passport');
 const session = require('express-session');
-const flash = require("connect-flash");
+const flash = require('connect-flash');
 const ejs = require('ejs');
-const dotenv = require('dotenv')
-
-
+const dotenv = require('dotenv');
 
 const app = express();
-dotenv.config()
+dotenv.config();
 
-const PORT  = process.env.PORT
+const PORT = process.env.PORT;
 
 //passport config:
-require('./config/passport')
-
-
-
-
+require('./config/passport');
 
 // Get all routes
 
-const indexRoutes = require("./routes/index");
-const authRoutes = require("./routes/auth");
-const changePasswordRoutes = require("./routes/changePassword");
-const contactRoutes = require("./routes/contact");
-const profileAndTeamRoutes = require("./routes/profileAndTeam");
-
-
-
+const indexRoutes = require('./routes/index');
+const authRoutes = require('./routes/auth');
+const changePasswordRoutes = require('./routes/changePassword');
+const contactRoutes = require('./routes/contact');
+const profileAndTeamRoutes = require('./routes/profileAndTeam');
 
 // view engine setup
 app.set('view engine', 'ejs');
 
 // PASSPORT CONFIGURATION
-app.use(session({
-  secret : 'mycon',
-  resave : true,
-  saveUninitialized : true
-}));
+app.use(
+	session({
+		secret: 'mycon',
+		resave: true,
+		saveUninitialized: true,
+	})
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
-
 
 app.set('views', path.join(__dirname, 'views'));
 
@@ -53,12 +45,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-app.use(session({
-  secret : 'mycon',
-  resave : true,
-  saveUninitialized : true
-}));
+app.use(
+	session({
+		secret: 'mycon',
+		resave: true,
+		saveUninitialized: true,
+	})
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -66,23 +59,19 @@ app.use(passport.session());
 // req flash
 app.use(flash());
 
-app.use((req,res,next)=> {
-    res.locals.success_msg = req.flash('success_msg');
-    res.locals.error_msg = req.flash('error_msg');
-    res.locals.error  = req.flash('error');
-    next();
-    })
-
-
-
-    app.use(bodyParser.urlencoded({extended: true}));
-
-
 app.use((req, res, next) => {
-    res.locals.currentUser = req.user;
-    next();
+	res.locals.success_msg = req.flash('success_msg');
+	res.locals.error_msg = req.flash('error_msg');
+	res.locals.error = req.flash('error');
+	next();
 });
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+	res.locals.currentUser = req.user;
+	next();
+});
 
 app.use('/', indexRoutes);
 app.use('/', authRoutes);
@@ -90,33 +79,28 @@ app.use('/', changePasswordRoutes);
 app.use('/', contactRoutes);
 app.use('/', profileAndTeamRoutes);
 
+app.get('*', (req, res, next) => {
+	res.render('error404');
+});
 
-app.get('*', (req, res, next)=>{
-  res.render('error404')
-})
-
-let mongoURL = ''
+let mongoURL = '';
 
 if (process.env.NODE_ENV === 'production') {
-    mongoURL = process.env.MONGO_URI_PROD
-    console.log('Running in Production environment');
+	mongoURL = process.env.MONGO_URI_PROD;
+	console.log('Running in Production environment');
 } else if (process.env.NODE_ENV === 'local') {
-    mongoURL = process.env.MONGO_URI_LOCAL
-    console.log('Running in local environment');
-    
+	mongoURL = process.env.MONGO_URI_LOCAL;
+	console.log('Running in local environment');
 } else {
-    console.log('ENVIRONMENT NOT SET CORRECTLY, PLEASE CHECK')
-
+	console.log('ENVIRONMENT NOT SET CORRECTLY, PLEASE CHECK');
 }
 
 // MONGOOSE CONNECTION
-mongoose.connect(mongoURL)
-    .then(() => {
-        app.listen(PORT, () => {
-            console.log(`connected to port: ${PORT} and dataBase ${mongoURL}`)
-        })
-
-    })
-    .catch(err => console.log('err'))
-
-
+mongoose
+	.connect(mongoURL)
+	.then(() => {
+		app.listen(PORT, () => {
+			console.log(`connected to port: ${PORT} and dataBase ${mongoURL}`);
+		});
+	})
+	.catch((err) => console.log('err'));
--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,26 @@
 const express = require('express');
 const router = express.Router();
-const {ensureAuthenticated} = require('../config/auth');
-
+const { ensureAuthenticated } = require('../config/auth');
 
 // get homepage
 router.get('/', async (req, res) => {
-  res.render('index');
+	res.render('index');
 });
 
-router.get('/realEstate', ensureAuthenticated,(req, res, next)=>{
-  res.render('realEstate')
-})
+router.get('/realEstate', (req, res, next) => {
+	res.render('realEstate');
+});
 
-router.get('/cashInvestment', ensureAuthenticated,(req, res, next)=>{
-  res.render('cashInvestment')
-})
+router.get('/cashInvestment', (req, res, next) => {
+	res.render('cashInvestment');
+});
 
-router.get('/cryptoInvestment', ensureAuthenticated,(req, res, next)=>{
-  res.render('cryptoInvestment')
-})
+router.get('/cryptoInvestment', (req, res, next) => {
+	res.render('cryptoInvestment');
+});
 
-router.get('/assetManagement', ensureAuthenticated,(req, res, next)=>{
-  res.render('assetManagement')
-})
-
-
-
-
-
-
-
-
-
-
+router.get('/assetManagement', (req, res, next) => {
+	res.render('assetManagement');
+});
 
 module.exports = router;
